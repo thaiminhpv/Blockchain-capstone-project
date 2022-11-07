26865c3f313715eb2e577b5f0c98a3d7a1438986
thaiminhpv <thaiminhpv@gmail.com>
2022-11-07 09:55:03 +0700

    downgrade to 0.4.17

--- contracts/Reserve.sol
+++ contracts/Reserve.sol
@@ -1,7 +1,7 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.4.17;
 
 contract Reserve {
-  constructor() public {
-  }
+    function Reserve() public {
+
+    }
 }
