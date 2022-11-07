4d06fe0926502130e46d4ca45af17cb77e9cd15c
thaiminhpv <thaiminhpv@gmail.com>
2022-11-07 09:55:14 +0700

    Revert "downgrade to 0.4.17"
    This reverts commit 26865c3f313715eb2e577b5f0c98a3d7a1438986.

--- contracts/Reserve.sol
+++ contracts/Reserve.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
 
 contract Reserve {
-    function Reserve() public {
-
-    }
+  constructor() public {
+  }
 }
