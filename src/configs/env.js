3e9c048d534baa227cfa8d0b812e13d22d0789fa
thaiminhpv <thaiminhpv@gmail.com>
2022-11-15 14:30:07 +0700

    add address at ganache-cli seed 5555

--- src/configs/env.js
+++ src/configs/env.js
@@ -6,7 +6,7 @@ const EnvConfig = {
   TOKEN_ABI: [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}],
   /* TODO: You should change these configurations with your deployed exchange contract instead */
   EXCHANGE_CONTRACT_ABI: exchange_abi.abi,
-  EXCHANGE_CONTRACT_ADDRESS: '0x3414aCC062f491210c69dE168A7379b1B9Eef8F4',
+  EXCHANGE_CONTRACT_ADDRESS: '0x0BBE5B854d8638f6785B22E6b61d3B35F74a755a',
   /* END TODO */
 
   TOKENS: [
@@ -19,12 +19,12 @@ const EnvConfig = {
     {
       "name": 'TokenA',
       "symbol": 'TKA',
-      address: '0x15e9C57B005411fd584efE7C6BfB7600620a7A4A',
+      address: '0x5111031586BD3ED0177C11F5A58981b012b30424',
     },
     {
       "name": 'TokenB',
       "symbol": 'TKB',
-      address: '0x5B036a8FF059fC23476c512fDD8e5077dD708898',
+      address: '0x0dAd3CdFa49aDd1dB0Db94c0c7EE343B894c93FF',
     },
     /* END TODO */
   ],
