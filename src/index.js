c82b2fc61a5d05a8e9bd387256138e0bb3682879
thaiminhpv <thaiminhpv@gmail.com>
2022-11-15 13:07:58 +0700

    refactor functions to arrow form

--- src/index.js
+++ src/index.js
@@ -1,73 +1,72 @@
 import { getExchangeRate } from "./services/networkService";
 import EnvConfig from "./configs/env";
 
+const initiateDropdown = () => {
+  let dropdownTokens = '';
+
+  EnvConfig.TOKENS.forEach((token) => {
+    dropdownTokens += `<div class="dropdown__item">${token.symbol}</div>`;
+  });
+
+  $('.dropdown__content').html(dropdownTokens);
+};
+
+const initiateSelectedToken = (srcSymbol, destSymbol) => {
+  $('#selected-src-symbol').html(srcSymbol);
+  $('#selected-dest-symbol').html(destSymbol);
+  $('#rate-src-symbol').html(srcSymbol);
+  $('#rate-dest-symbol').html(destSymbol);
+  $('#selected-transfer-token').html(srcSymbol);
+};
+
+const updateExchangeRate = async (srcSymbol, destSymbol, srcAmount=1) => {
+  const srcToken = findTokenBySymbol(srcSymbol);
+  const destToken = findTokenBySymbol(destSymbol);
+  const srcAmountFull = (srcAmount * 1e18).toString();
+
+  $('#rate-src-symbol').html(srcSymbol);
+  $('#rate-dest-symbol').html(destSymbol);
+  try {
+    const exchangeRate = await getExchangeRate(srcToken.address, destToken.address, srcAmountFull);
+    console.log(`Exchange rate of ${srcSymbol}->${destSymbol}: ${exchangeRate}`);
+    const rate = exchangeRate / 1e18
+    $('#exchange-rate').html(rate);
+    return rate;
+  } catch (error) {
+    console.log(error);
+    $('#exchange-rate').html(0);
+    return 0;
+  }
+};
+
+const refreshTokenRate = async () => {
+  const srcSymbol = $('#selected-src-symbol').html();
+  const destSymbol = $('#selected-dest-symbol').html();
+  const srcAmount = parseInt($('#swap-source-amount').val());
+
+  const rate = await updateExchangeRate(srcSymbol, destSymbol, srcAmount);
+  $('#swap-dest-amount').html(rate * srcAmount);
+  // FIXME: Update dest amount when srcToken is same as destToken
+};
+
+const initiateDefaultRate = (srcSymbol, destSymbol) => {
+  refreshTokenRate();
+};
+
+const findTokenBySymbol = symbol => EnvConfig.TOKENS.find(token => token.symbol === symbol);
+
+const initiateProject = () => {
+  const defaultSrcSymbol = EnvConfig.TOKENS[0].symbol;
+  const defaultDestSymbol = EnvConfig.TOKENS[1].symbol;
+
+  initiateDropdown();
+  initiateSelectedToken(defaultSrcSymbol, defaultDestSymbol);
+  initiateDefaultRate(defaultSrcSymbol, defaultDestSymbol);
+};
+
 $(function () {
   initiateProject();
 
-  function initiateProject() {
-    const defaultSrcSymbol = EnvConfig.TOKENS[0].symbol;
-    const defaultDestSymbol = EnvConfig.TOKENS[1].symbol;
-    
-    initiateDropdown();
-    initiateSelectedToken(defaultSrcSymbol, defaultDestSymbol);
-    initiateDefaultRate(defaultSrcSymbol, defaultDestSymbol);
-  }
-  
-  function initiateDropdown() {
-    let dropdownTokens = '';
-    
-    EnvConfig.TOKENS.forEach((token) => {
-      dropdownTokens += `<div class="dropdown__item">${token.symbol}</div>`;
-    });
-    
-    $('.dropdown__content').html(dropdownTokens);
-  }
-  
-  function initiateSelectedToken(srcSymbol, destSymbol) {
-    $('#selected-src-symbol').html(srcSymbol);
-    $('#selected-dest-symbol').html(destSymbol);
-    $('#rate-src-symbol').html(srcSymbol);
-    $('#rate-dest-symbol').html(destSymbol);
-    $('#selected-transfer-token').html(srcSymbol);
-  }
-  
-  function initiateDefaultRate(srcSymbol, destSymbol) {
-    refreshTokenRate();
-  }
-
-  function findTokenBySymbol(symbol) {
-    return EnvConfig.TOKENS.find(token => token.symbol === symbol);
-  }
-
-  async function updateExchangeRate(srcSymbol, destSymbol, srcAmount=1) {
-    const srcToken = findTokenBySymbol(srcSymbol);
-    const destToken = findTokenBySymbol(destSymbol);
-    const srcAmountFull = (srcAmount * 1e18).toString();
-
-    $('#rate-src-symbol').html(srcSymbol);
-    $('#rate-dest-symbol').html(destSymbol);
-    try {
-      const exchangeRate = await getExchangeRate(srcToken.address, destToken.address, srcAmountFull);
-      console.log("exchangeRate: ", exchangeRate);
-      const rate = exchangeRate / 1e18
-      $('#exchange-rate').html(rate);
-      return rate;
-    } catch (error) {
-      console.log(error);
-      $('#exchange-rate').html(0);
-      return 0;
-    }
-  }
-
-  async function refreshTokenRate() {
-    const srcSymbol = $('#selected-src-symbol').html();
-    const destSymbol = $('#selected-dest-symbol').html();
-    const srcAmount = parseInt($('#swap-source-amount').val());
-
-    const rate = await updateExchangeRate(srcSymbol, destSymbol, srcAmount);
-    $('#swap-dest-amount').html(rate * srcAmount);
-  }
-  
   // On changing token from dropdown.
   $(document).on('click', '.dropdown__item', function () {
     const selectedSymbol = $(this).html();
